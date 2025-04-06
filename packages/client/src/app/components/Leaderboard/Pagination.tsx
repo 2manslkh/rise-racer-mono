ea9892d494fa7e9c42d97780c08c5be351199eca
Eric Mong <m.changhsi@gmail.com>
2025-04-06 14:52:17 +0800

    feat: add pagination for leaderboard

---
+++ packages/client/src/app/components/Leaderboard/Pagination.tsx
@@ -0,0 +1,150 @@
+import { Dispatch, SetStateAction, useEffect } from "react";
+
+type PaginationButtonProps = React.HTMLAttributes<HTMLDivElement> & {
+  selected?: boolean;
+  children: React.ReactNode;
+};
+
+const PaginationButton = ({
+  selected,
+  children,
+  ...props
+}: PaginationButtonProps) => {
+  return (
+    <div
+      {...props}
+      className={`w-6 h-6 bg-[#5900A7] flex items-center justify-center box-border rounded-lg ${selected ? "border border-solid border-white" : ""} font-inter text-xs font-extrabold text-white`}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Pagination = ({
+  currentPage,
+  setCurrentPage,
+  totalNumberOfData,
+  pageSize,
+}: {
+  currentPage: number;
+  setCurrentPage: Dispatch<SetStateAction<number>>;
+  totalNumberOfData: number;
+  pageSize: number;
+}) => {
+  const totalPages = Math.ceil(totalNumberOfData / pageSize);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLElement;
+    const item = target.closest("[data-id]") as HTMLElement | null;
+    const id = item?.getAttribute("data-id");
+
+    if (id) {
+      if (id === "PREV") {
+        if (currentPage === 1) return;
+        setCurrentPage(currentPage - 1);
+      } else if (id === "NEXT") {
+        if (currentPage === totalPages) return;
+        setCurrentPage(currentPage + 1);
+      } else {
+        setCurrentPage(+id);
+      }
+    }
+  };
+
+  return (
+    <div
+      className="relative w-full flex items-center justify-center gap-1"
+      onClick={handleClick}
+    >
+      <PaginationButton data-id={"PREV"}>
+        <svg
+          width="10"
+          height="10"
+          viewBox="0 0 10 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1 5.86603C0.333333 5.48113 0.333333 4.51887 1 4.13397L7.75 0.236859C8.41667 -0.148041 9.25 0.333085 9.25 1.10289V8.89711C9.25 9.66691 8.41667 10.148 7.75 9.76314L1 5.86603Z"
+            fill="white"
+          />
+        </svg>
+      </PaginationButton>
+      {totalPages <= 5 ? (
+        <>
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+            <PaginationButton selected={currentPage === page} data-id={page}>
+              {page}
+            </PaginationButton>
+          ))}
+        </>
+      ) : (
+        <>
+          {currentPage >= totalPages - 3 ? (
+            <>
+              {/* Means the last page is relatively far away from the current page 
+          So render first 3 pages ... then last page*/}
+              <PaginationButton selected data-id={currentPage}>
+                {currentPage}
+              </PaginationButton>
+              <PaginationButton selected data-id={currentPage + 1}>
+                {currentPage + 1}
+              </PaginationButton>
+              <PaginationButton selected data-id={currentPage + 2}>
+                {currentPage + 2}
+              </PaginationButton>
+              <PaginationButton>...</PaginationButton>
+              <PaginationButton selected data-id={totalPages}>
+                {totalPages}
+              </PaginationButton>
+            </>
+          ) : (
+            <>
+              {/* Means the last page is relatively near from the current page
+           So render first page ... then last 3 page */}
+              <PaginationButton selected={1 === currentPage} data-id={1}>
+                1
+              </PaginationButton>
+              <PaginationButton>...</PaginationButton>
+              <PaginationButton
+                selected={totalPages - 2 === currentPage}
+                data-id={totalPages - 2}
+              >
+                {totalPages - 2}
+              </PaginationButton>
+              <PaginationButton
+                selected={totalPages - 1 === currentPage}
+                data-id={totalPages - 1}
+              >
+                {totalPages - 1}
+              </PaginationButton>
+              <PaginationButton
+                selected={totalPages === currentPage}
+                data-id={totalPages}
+              >
+                {totalPages}
+              </PaginationButton>
+            </>
+          )}
+        </>
+      )}
+
+      <PaginationButton data-id={"NEXT"}>
+        <svg
+          width="10"
+          height="10"
+          viewBox="0 0 10 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M9 5.86603C9.66667 5.48113 9.66667 4.51887 9 4.13397L2.25 0.236859C1.58333 -0.148041 0.75 0.333085 0.75 1.10289V8.89711C0.75 9.66691 1.58333 10.148 2.25 9.76314L9 5.86603Z"
+            fill="white"
+          />
+        </svg>
+      </PaginationButton>
+    </div>
+  );
+};
+
+export default Pagination;
--- packages/client/src/app/components/Leaderboard/index.tsx
+++ packages/client/src/app/components/Leaderboard/index.tsx
@@ -1,69 +1,24 @@
 import { User } from "@/app/page";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { MOCK_DATA } from "./data";
+import Pagination from "./Pagination";
 
-type Position = {
+export type Position = {
   rank: number;
   player: string;
   pfp: string;
   score: number;
 };
 
-const MOCK_DATA: Position[] = [
-  {
-    rank: 1,
-    player: "Anthony Mega Storm",
-    pfp: "./PFP.svg",
-    score: 999,
-  },
-  {
-    rank: 2,
-    player: "Anthony Mega Storm",
-    pfp: "./PFP.svg",
-    score: 998,
-  },
-  {
-    rank: 3,
-    player: "Anthony Mega Storm",
-    pfp: "./PFP.svg",
-    score: 997,
-  },
-  {
-    rank: 4,
-    player: "Anthony Mega Storm",
-    pfp: "./PFP.svg",
-    score: 995,
-  },
-  {
-    rank: 5,
-    player: "Anthony Mega Storm",
-    pfp: "./PFP.svg",
-    score: 993,
-  },
-  {
-    rank: 6,
-    player: "Anthony Mega Storm",
-    pfp: "./PFP.svg",
-    score: 992,
-  },
-  {
-    rank: 7,
-    player: "Anthony Mega Storm",
-    pfp: "./PFP.svg",
-    score: 991,
-  },
-  {
-    rank: 8,
-    player: "Anthony Mega Storm",
-    pfp: "./PFP.svg",
-    score: 990,
-  },
-];
-
-const RenderLeaderboardRow = (data: Position, index: number) => {
+const RenderLeaderboardRow = (
+  data: Position,
+  totalNumber: number,
+  index: number
+) => {
   return (
     <div
-      className={`relative w-full flex items-center px-4 py-2 ${index !== ROWS_PER_PAGE - 1 ? "border-b border-solid border-white" : ""}`}
+      className={`relative w-full flex items-center px-4 py-2 ${index !== totalNumber - 1 ? "border-b border-solid border-white" : ""}`}
     >
       <div className="flex flex-1">
         <p className="font-inter font-bold text-base text-white">{data.rank}</p>
@@ -88,7 +43,15 @@ const RenderLeaderboardRow = (data: Position, index: number) => {
 const ROWS_PER_PAGE = 6;
 
 const Leaderboard = ({ user }: { user: User }) => {
+  const [data, setData] = useState<Position[]>(MOCK_DATA);
+  const [displayData, setDisplayData] = useState<Position[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
+
+  useEffect(() => {
+    setDisplayData(
+      data.slice(ROWS_PER_PAGE * (currentPage - 1), ROWS_PER_PAGE * currentPage)
+    );
+  }, [currentPage]);
 
   return (
     <div className="relative w-full h-full bg-[#2A004F] flex flex-col py-4 px-2 items-center gap-4">
@@ -102,7 +65,7 @@ const Leaderboard = ({ user }: { user: User }) => {
       </p>
 
       {/* Leaderboard */}
-      <div className="relative w-full rounded-xl p-2 bg-[#8322F9]">
+      <div className="relative w-full rounded-xl p-2 bg-[#8322F9] flex flex-col gap-1">
         <div className="relative w-full rounded-xl py-2 px-4 bg-[#2A004F] flex items-center">
           <div className="flex flex-1">
             <p className="font-inter font-bold text-base text-white">Rank</p>
@@ -115,10 +78,16 @@ const Leaderboard = ({ user }: { user: User }) => {
           </div>
         </div>
 
-        {MOCK_DATA.slice(
-          ROWS_PER_PAGE * (currentPage - 1),
-          ROWS_PER_PAGE * currentPage
-        ).map((data, index) => RenderLeaderboardRow(data, index))}
+        {displayData.map((_data, index) =>
+          RenderLeaderboardRow(_data, displayData.length, index)
+        )}
+
+        <Pagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          totalNumberOfData={data.length}
+          pageSize={ROWS_PER_PAGE}
+        />
       </div>
 
       {/* Your ranking */}
