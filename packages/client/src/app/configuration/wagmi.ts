8e400bfdeb19d8f18ca5507891bfc19da4da0bd6
Eric Mong <m.changhsi@gmail.com>
2025-04-06 14:59:07 +0800

    feat: add rise testnet

--- packages/client/src/app/configuration/wagmi.ts
+++ packages/client/src/app/configuration/wagmi.ts
@@ -1,6 +1,6 @@
 import { cookieStorage, createStorage } from "wagmi";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { mainnet, arbitrum } from "@reown/appkit/networks";
+import { mainnet, defineChain } from "@reown/appkit/networks";
 import type { AppKitNetwork } from "@reown/appkit/networks";
 import { ENVIRONMENT } from "./environment";
 
@@ -9,6 +9,27 @@ export const projectId = ENVIRONMENT.REOWN_PROJECT_ID;
 if (!projectId) {
   throw new Error("Project ID is not defined");
 }
+
+const riseTestnet = defineChain({
+  id: 11155931,
+  caipNetworkId: "eip155:11155931",
+  chainNamespace: "eip155",
+  name: "RISE Testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://testnet.riselabs.xyz"],
+      webSocket: ["wss://testnet.riselabs.xyz/ws"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://testnet.explorer.riselabs.xyz" },
+  },
+});
 
 export const networks = [mainnet] as [AppKitNetwork, ...AppKitNetwork[]];
 
