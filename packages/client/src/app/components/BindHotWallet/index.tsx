1c728f2dd044286dfc92fef398d9fb863f3bd776
Eric Mong <m.changhsi@gmail.com>
2025-04-09 15:58:35 +0800

    feat: update hot wallet modal styling

--- packages/client/src/app/components/BindHotWallet/index.tsx
+++ packages/client/src/app/components/BindHotWallet/index.tsx
@@ -2,13 +2,27 @@ const BindHotWallet = ({ handleClick }: { handleClick: () => void }) => {
   return (
     <>
       <div className="absolute inset-0 bg-black/30 backdrop-blur-sm transition-opacity duration-300 z-3 h-screen max-h-[896px]"></div>
-      <div className="fixed flex flex-col items-center justify-center w-[90%] max-w-[300px] h-content bg-white z-3 p-4 rounded-xl top-1/2 left-1/2 transform -translate-1/2">
-        <p>Bind your hot wallet</p>
-        <p>
-          This is to allow you to top up gas into this wallet for
-          transactionless interactions with our game!
+      <div className="fixed flex flex-col items-center justify-center w-[90%] max-w-[350px] h-content bg-white z-3 p-4 rounded-xl top-1/2 left-1/2 transform -translate-1/2 gap-2">
+        <p
+          className="font-zen text-white text-2xl relative text-center"
+          style={{
+            WebkitTextStroke: "1.5px #74007E",
+          }}
+        >
+          BIND HOT WALLET
         </p>
-        <button onClick={handleClick}>Bind</button>
+        <p className="font-inter text-black">INSERT HOT WALLET ADDRESS</p>
+        <p className="font-inter text-black">
+          Hot Wallet will allow you to make signless transactions through the
+          consumption of gas from this wallet. Do make sure that you top up some
+          value here!
+        </p>
+        <button
+          onClick={handleClick}
+          className="w-full bg-[#5700A3] py-2 rounded-xl"
+        >
+          Bind
+        </button>
       </div>
     </>
   );
