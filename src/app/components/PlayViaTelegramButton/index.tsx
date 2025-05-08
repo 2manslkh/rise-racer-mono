5bdf917509b30fcbf8f30da93ff7a93d24f5299f
wagmitt <wagmitt@gmail.com>
2025-05-08 18:05:19 +0700

    refactor

--- src/app/components/PlayViaTelegramButton/index.tsx
+++ src/app/components/PlayViaTelegramButton/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useHotWallet } from "@/app/context/HotWalletContext";
+import { useTMA } from "@/app/context/TelegramContext";
 import React from "react";
 
 type PlayViaTelegramButtonProps = Record<string, never>;
@@ -7,12 +9,16 @@ type PlayViaTelegramButtonProps = Record<string, never>;
 const PlayViaTelegramButton: React.FC<
   PlayViaTelegramButtonProps
 > = (/*{ onClick }*/) => {
+  const { loadHotWallet } = useHotWallet();
+  const { authenticate } = useTMA();
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <button
-        onClick={() => {
+        onClick={async () => {
           // TODO: Implement Telegram linking/login functionality
           console.log("Play via Telegram button clicked");
+          await authenticate();
+          loadHotWallet();
         }}
         className="px-6 py-3 text-lg font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50"
       >
